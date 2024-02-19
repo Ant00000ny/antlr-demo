263803e93e47b5c6064c0b162c22e09b4ae83f2b
Ant00000ny <Ant00000ny@icloud.com>
2024-02-19 16:18:08 +0800

    basic apply

--- src/main/kotlin/com/isekaiofficial/antlrdemo/Main.kt
+++ src/main/kotlin/com/isekaiofficial/antlrdemo/Main.kt
@@ -1,7 +1,18 @@
 package com.isekaiofficial.antlrdemo
 
+import SeaLexer
+import SeaParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+
+
 class Main
 
 fun main() {
-    println("123")
+    val lexer = SeaLexer(CharStreams.fromString("5838 + 44 - 3"))
+    val tokens = CommonTokenStream(lexer)
+    val parser = SeaParser(tokens)
+    val tree = parser.fileStat()
+
+    println(tree)
 }
